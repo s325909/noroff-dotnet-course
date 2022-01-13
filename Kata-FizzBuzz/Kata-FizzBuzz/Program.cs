fac4aad215c3551cb409011835bbc83d5d69e16f
s325909 <jaso_cyril@hotmail.com>
2022-01-13 18:32:16 +0100

    Made ConvertFizzBuzz() method

--- Kata-FizzBuzz/Kata-FizzBuzz/Program.cs
+++ Kata-FizzBuzz/Kata-FizzBuzz/Program.cs
@@ -7,6 +7,18 @@ namespace Kata_FizzBuzz
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(ConvertFizzBuzz(30));
+
+            Console.ReadKey();
+        }
+
+        static string ConvertFizzBuzz(int i)
+        {
+            if (i % 15 == 0) return "FizzBuzz";
+            else if (i % 3 == 0) return "Fizz";
+            else if (i % 5 == 0) return "Buzz";
+            else return i.ToString();
         }
     }
 }
