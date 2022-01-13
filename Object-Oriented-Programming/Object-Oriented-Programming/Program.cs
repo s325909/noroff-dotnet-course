95975560fbfd68ce9e419b3c328fca1e2ca1ae4a
s325909 <jaso_cyril@hotmail.com>
2022-01-13 18:45:55 +0100

    Object Oriented Programming

---
+++ Object-Oriented-Programming/Object-Oriented-Programming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Object_Oriented_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
