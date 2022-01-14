75b67669d6e63647c56d2deedbc058d1eac436ae
s325909 <jaso_cyril@hotmail.com>
2022-01-14 10:00:45 +0100

    Freddy Friday loop from current year to 3000

--- Kata-Freddy-Friday/Kata-Freddy-Friday/Program.cs
+++ Kata-Freddy-Friday/Kata-Freddy-Friday/Program.cs
@@ -9,7 +9,14 @@ namespace Kata_Freddy_Friday
         {
             Console.WriteLine("Hello World!");
 
-            var year = DateTime.Today.Year + 1;
+            var year = DateTime.Today.Year;
+
+            while (year < 3000)
+            {
+                FreddyFriday(year);
+                year++;
+            }
+            
 
             // int year = 2025;
 
@@ -19,6 +26,43 @@ namespace Kata_Freddy_Friday
             Console.WriteLine(year + " / " + month + " / " + day);
             **/
 
+            /**
+            for (int i = 1; i <= 12; i++)
+            {
+                DateTime date = new DateTime(DateTime.Today.Year, i, 13);
+                if (date.DayOfWeek == DayOfWeek.Friday)
+                {
+                    Console.WriteLine(date.ToString("dd\\t\\h MMMM yyyy") + " is a Friday");
+                }
+            }
+            **/
+
+
+            // FreddyFriday(year);
+
+
+            FreddyFridayThisYear(year);
+
+
+            Console.ReadKey();
+        }
+
+        static void FreddyFriday(int year)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                // DateTime date = new DateTime(DateTime.Today.Year, i, 13);
+                DateTime date = new DateTime(year, i, 13);
+
+                if (date.DayOfWeek == DayOfWeek.Friday)
+                {
+                    Console.WriteLine(date.ToString("dd\\t\\h MMMM yyyy") + " is a Friday");
+                }
+            }
+        }
+
+        static void FreddyFridayThisYear(int year)
+        {
             foreach (var month in Enumerable.Range(1, 12))
             {
                 foreach (var day in Enumerable.Range(1, DateTime.DaysInMonth(year, month))
@@ -26,19 +70,12 @@ namespace Kata_Freddy_Friday
                 {
                     // Console.WriteLine(day);
 
-                    // if (day.Contains("fredag mai") && day.Contains("13")) Console.WriteLine("FREDDY FRIDAY!");
-
-                    if (day.Contains("fredag mai") && day.Contains("13")) Console.WriteLine(day);
+                    if (day.Contains("fredag mai") && day.Contains("13")) Console.WriteLine(day + " " + year);
 
                 }
                 // Console.WriteLine("Press a key to continue...");
                 // Console.ReadKey(true);
             }
-
-
-
-
-            Console.ReadKey();
         }
     }
 }
