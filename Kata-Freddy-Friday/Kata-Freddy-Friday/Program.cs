36f20f3a90c2ec61b0da5e99b5cdfd5ee9ed72cb
s325909 <jaso_cyril@hotmail.com>
2022-01-14 10:30:38 +0100

    Removed initial method

--- Kata-Freddy-Friday/Kata-Freddy-Friday/Program.cs
+++ Kata-Freddy-Friday/Kata-Freddy-Friday/Program.cs
@@ -7,7 +7,7 @@ namespace Kata_Freddy_Friday
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Freddy Friday!");
 
             var year = DateTime.Today.Year;
 
@@ -17,33 +17,6 @@ namespace Kata_Freddy_Friday
                 year++;
             }
             
-
-            // int year = 2025;
-
-            /**
-            var month = DateTime.Today.Month;
-            var day = DateTime.Today.Day;
-            Console.WriteLine(year + " / " + month + " / " + day);
-            **/
-
-            /**
-            for (int i = 1; i <= 12; i++)
-            {
-                DateTime date = new DateTime(DateTime.Today.Year, i, 13);
-                if (date.DayOfWeek == DayOfWeek.Friday)
-                {
-                    Console.WriteLine(date.ToString("dd\\t\\h MMMM yyyy") + " is a Friday");
-                }
-            }
-            **/
-
-
-            // FreddyFriday(year);
-
-
-            FreddyFridayThisYear(year);
-
-
             Console.ReadKey();
         }
 
@@ -58,23 +31,6 @@ namespace Kata_Freddy_Friday
                 {
                     Console.WriteLine(date.ToString("dd\\t\\h MMMM yyyy") + " is a Friday");
                 }
-            }
-        }
-
-        static void FreddyFridayThisYear(int year)
-        {
-            foreach (var month in Enumerable.Range(1, 12))
-            {
-                foreach (var day in Enumerable.Range(1, DateTime.DaysInMonth(year, month))
-                          .Select(day => new DateTime(year, month, day).ToString("dddd MMM d")))
-                {
-                    // Console.WriteLine(day);
-
-                    if (day.Contains("fredag mai") && day.Contains("13")) Console.WriteLine(day + " " + year);
-
-                }
-                // Console.WriteLine("Press a key to continue...");
-                // Console.ReadKey(true);
             }
         }
     }
