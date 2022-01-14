0188b095ff25a57715bc22b68717561f721bb37d
s325909 <jaso_cyril@hotmail.com>
2022-01-14 09:03:28 +0100

    Kata-Freddy-Friday

---
+++ Kata-Freddy-Friday/Kata-Freddy-Friday/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata_Freddy_Friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
