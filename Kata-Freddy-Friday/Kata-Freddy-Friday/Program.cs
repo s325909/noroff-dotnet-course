ebf147010e7fc1048816ebbb82a1c6563e8f914f
s325909 <jaso_cyril@hotmail.com>
2022-01-14 09:33:31 +0100

    loops through the year and prints friday the 13ths

--- Kata-Freddy-Friday/Kata-Freddy-Friday/Program.cs
+++ Kata-Freddy-Friday/Kata-Freddy-Friday/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Kata_Freddy_Friday
 {
@@ -7,6 +8,37 @@ namespace Kata_Freddy_Friday
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var year = DateTime.Today.Year + 1;
+
+            // int year = 2025;
+
+            /**
+            var month = DateTime.Today.Month;
+            var day = DateTime.Today.Day;
+            Console.WriteLine(year + " / " + month + " / " + day);
+            **/
+
+            foreach (var month in Enumerable.Range(1, 12))
+            {
+                foreach (var day in Enumerable.Range(1, DateTime.DaysInMonth(year, month))
+                          .Select(day => new DateTime(year, month, day).ToString("dddd MMM d")))
+                {
+                    // Console.WriteLine(day);
+
+                    // if (day.Contains("fredag mai") && day.Contains("13")) Console.WriteLine("FREDDY FRIDAY!");
+
+                    if (day.Contains("fredag mai") && day.Contains("13")) Console.WriteLine(day);
+
+                }
+                // Console.WriteLine("Press a key to continue...");
+                // Console.ReadKey(true);
+            }
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
