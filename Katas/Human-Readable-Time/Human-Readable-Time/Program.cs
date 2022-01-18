91a2af2cd8482614a09b25ca3aad7838266dcb87
s325909 <jaso_cyril@hotmail.com>
2022-01-18 09:05:41 +0100

    Initial Project

---
+++ Katas/Human-Readable-Time/Human-Readable-Time/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Human_Readable_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
