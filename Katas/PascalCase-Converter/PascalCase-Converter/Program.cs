80fdc12c1d93a5e2338a70d70c2f940ef8836430
s325909 <jaso_cyril@hotmail.com>
2022-01-19 09:23:24 +0100

    Feat: PascalCase Converter

---
+++ Katas/PascalCase-Converter/PascalCase-Converter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace PascalCase_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string str = "The quick brown fox jumped over the lazy dog";
+            ConvertPascalCase(str);
+        }
+
+        private static void ConvertPascalCase(string str)
+        {
+            // string s = str.ToLower();
+            string[] words = str.ToLower().Split(" ");
+
+            StringBuilder sb = new();
+
+            foreach (string word in words) 
+            {
+                sb.Append(char.ToUpper(word[0]) + word[1..].ToLower());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
