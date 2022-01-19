b05ff6c0f4c170e2d21ca7abbc57a949da541129
s325909 <jaso_cyril@hotmail.com>
2022-01-19 09:57:06 +0100

    Chore: clean code

--- Katas/PascalCase-Converter/PascalCase-Converter/Program.cs
+++ Katas/PascalCase-Converter/PascalCase-Converter/Program.cs
@@ -8,32 +8,22 @@ namespace PascalCase_Converter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             string str = "The quick brown fox jumped over the lazy dog";
-            // ConvertPascalCase(str);
+            ConvertPascalCase(str);
 
-            string str2 = "pineapple on pizza";
-            // ConvertPascalCase(str2);
-
-            string str3 = "the quick   brown FOX! jUmpED   OVER the   lazy. dog";
-            ConvertPascalCase(str3);
+            string str2 = "the <quick>   brown FOX! jUmpED   OVER the   lazy. dog";
+            ConvertPascalCase(str2);
         }
 
         private static void ConvertPascalCase(string str)
         {
-            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
-
-            // string s = Regex.Replace(str, @"\s+", " ");
-            string[] words = rgx.Replace(str, " ").ToLower().Split(" ");
-
             StringBuilder sb = new();
+            Regex rgx = new("[^a-zA-Z0-9 -]");
+            string[] words = rgx.Replace(str, " ").ToLower().Split(" ");
 
             foreach (string word in words)
             {
-                // string s = Regex.Replace(word, @"\s+", String.Empty);
                 if (word.Length > 0) sb.Append(char.ToUpper(word[0]) + word[1..].ToLower());
-                // Console.WriteLine(s.Length);
             }
             
             Console.WriteLine(sb.ToString());
